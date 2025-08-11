6bc6ccd372e7bea96f6ddbb6681f99ed84d82077
PradeepkumarTest <143208848+PradeepkumarTest@users.noreply.github.com>
2025-08-11 10:37:46 +0530

    Update evennumber.java
    just add comments

--- DemoProgram/src/evennumber.java
+++ DemoProgram/src/evennumber.java
@@ -4,7 +4,7 @@ public class evennumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//scanner class
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter value of n:");
 		
